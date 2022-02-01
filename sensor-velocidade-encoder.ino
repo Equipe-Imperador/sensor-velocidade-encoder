5a50d7f94d972ae927c539579633290f26c9fb98
lucasrantunes <lucasribeiroantunes2013@gmail.com>
2022-01-31 22:23:16 -0300

    Cálculo do período e frequência

--- sensor-velocidade-encoder.ino
+++ sensor-velocidade-encoder.ino
@@ -1,14 +1,37 @@
 int pino_sensor = 8; // Pino digital utilizado pelo sensor
-   
+int sinal = 0;
+int ultimo_sinal = 0;
+double tempo_ultimo_sinal = 0;
+float periodo = 0; // [segundos]
+float frequencia = 0; // [RPM]
+
 void setup(){  
-  pinMode(pino_sensor, INPUT); // Define o pino como entrada
   Serial.begin(9600);
+  pinMode(pino_sensor, INPUT); // Define o pino como entrada
+  Serial.println("Periodo,Frequencia,tempo");
 }  
    
 void loop(){
-  if (digitalRead(pino_sensor) == HIGH){ // Se a leitura do pino for igual a low, faz
-        Serial.println("HIGH");
-  }else{//SENÃO, FAZ
-        Serial.println("LOW");
-  }    
+  // lê o sinal atual do sensor
+  sinal = digitalRead(pino_sensor);
+
+  // se houve uma mudança (tá em furo), significa que houve um ciclo, logo calcula o período e a frequência
+  if(sinal != ultimo_sinal){
+    // tempo atual do arduino
+    double tempo_sinal_atual = millis();
+
+    // intervalo de tempo entre a última passagem do furo e a atual (período)
+    periodo = (tempo_sinal_atual - tempo_ultimo_sinal)/1000;
+    // frequência em Hz = 1/periodo, frequencia em RPM = 60*frequência em Hz
+    frequencia = 60*(1/periodo);
+
+    // armazena as últimas medições para o cálculo seguinte
+    tempo_ultimo_sinal = tempo_sinal_atual;
+    ultimo_sinal = sinal;
+    Serial.print(periodo);
+    Serial.print(",");
+    Serial.print(frequencia);
+    Serial.print(",");
+    Serial.println(tempo_sinal_atual);
+  }
 }
