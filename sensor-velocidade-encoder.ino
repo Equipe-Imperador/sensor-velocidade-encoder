6f900bc6a1f00979ae92a41666a5d8c9ab4b18ba
lucasrantunes <lucasribeiroantunes2013@gmail.com>
2022-02-10 13:22:03 -0300

    Alteração na unidade de medida

--- sensor-velocidade-encoder.ino
+++ sensor-velocidade-encoder.ino
@@ -2,7 +2,7 @@ int pino_sensor = 8; // Pino digital utilizado pelo sensor
 int sinal = 0;
 int ultimo_sinal = 0;
 double tempo_ultimo_sinal = 0;
-float periodo = 0; // [segundos]
+float periodo = 0; // [ms]
 float frequencia = 0; // [RPM]
 
 void setup(){  
@@ -18,14 +18,14 @@ void loop(){
   // se houve uma mudança (tá em furo), significa que houve um ciclo, logo calcula o período e a frequência
   if(sinal != ultimo_sinal){
     // tempo atual do arduino
-    double tempo_sinal_atual = millis();
+    float tempo_sinal_atual = millis();
 
     // intervalo de tempo entre a última passagem do furo e a atual (período)
-    periodo = (tempo_sinal_atual - tempo_ultimo_sinal)/1000;
+    periodo = (tempo_sinal_atual - tempo_ultimo_sinal);
     // frequência em Hz = 1/periodo, frequencia em RPM = 60*frequência em Hz
     // Além disso, verifica se o perído é diferente de zero, caso seja igual mantém a leitura passada
     if(periodo != 0){
-      frequencia = 60*(1/periodo);
+      frequencia = 60*(1000/periodo);
     }
 
     // armazena as últimas medições para o cálculo seguinte
